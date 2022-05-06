269fddcd7aeb30d7251b6786065b821f64dc01e9
Joshua Abbey <Joshuaabbey2022@gmail.com>
2022-05-06 16:15:43 +0100

    enabled TLS and certs

--- main.go
+++ main.go
@@ -1,14 +1,14 @@
 package main
 
 import (
-	"log"
+	"fmt"
 	"net/http"
 )
 
 func main() {
 	fileServer := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fileServer)
-	if err := http.ListenAndServe(":80", nil); err != nil {
-		log.Fatal(err)
+	if err := http.ListenAndServeTLS(":443", "./static/certs/certificate.crt", "./static/certs/private.key", nil); err != nil {
+		fmt.Println(err)
 	}
 }
