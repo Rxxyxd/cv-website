0c2dc4953657d0d7de061e9a3e2ec66f2171c479
Joshua Abbey <Joshuaabbey2022@gmail.com>
2022-05-11 00:41:37 +0100

    removed login/signup functions since it wasn't functioning properly, adjusted the html/css, added a blog page and removed and added neccesary images

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/rxxyxd/cv-website
 
 go 1.18
-
-require golang.org/x/crypto v0.0.0-20220507011949-2cf3adece122
--- main.go
+++ main.go
@@ -1,28 +1,14 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
 	"net/http"
 )
 
-var db *sql.DB
-
 func main() {
-	http.HandleFunc("/login", login)
-	http.HandleFunc("/signup", signUp)
-	initDB()
 	fileServer := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fileServer)
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		fmt.Println(err)
-	}
-}
-
-func initDB() {
-	var err error
-	db, err = sql.Open("postgres", "dbname=jacom sslmode=disable")
-	if err != nil {
-		panic(err)
 	}
 }
